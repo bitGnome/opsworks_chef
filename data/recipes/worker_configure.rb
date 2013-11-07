3e232f285cbfcd818d12494db7e0d197ebf6b144
Brett Piatt <brett@betterdoctor.com>
2013-11-07 09:04:50 -0800

    minor tweaks

--- data/recipes/worker_configure.rb
+++ data/recipes/worker_configure.rb
@@ -1,12 +1,12 @@
-# Cookbook Name:: betterdoctor-data
-# Recipe:: worker
+# Cookbook Name:: data
+# Recipe:: worker_configure
 #
 # Copyright 2012,2013 BetterDoctor
 #
 # All rights reserved - Do Not Redistribute
 
 # rsyslog configuration
-cookbook_file "/home/ubunutfoosh.txt" do
+cookbook_file "/home/ubuntu/foosh.txt" do
   source "testing.txt"
   mode '0644'
 end
--- data/recipes/worker_deploy.rb
+++ data/recipes/worker_deploy.rb
@@ -1,3 +1,10 @@
+# Cookbook Name:: data
+# Recipe:: worker_deploy
+#
+# Copyright 2012,2013 BetterDoctor
+#
+# All rights reserved - Do Not Redistribute
+
 # deploy our application
 deploy_revision 'betterdoctor-data' do
   repository  'git@github.com:betterdoctor/betterdoctor-data.git'
